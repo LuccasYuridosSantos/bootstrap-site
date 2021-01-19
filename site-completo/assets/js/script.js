9d9f1b50c2b1652fe15196d4f110ed3b30cfbfd7
Luccas Yuri dos Santos <luccas.yui@hotmail.com>
2021-01-19 20:11:16 -0300

    Corrigindo responsividade no iframe

--- site-completo/assets/js/script.js
+++ site-completo/assets/js/script.js
@@ -9,8 +9,8 @@ function validaNome() {
     let txtNome = document.querySelector('#txtNome')
     if (nome.value.length < 3) {
         txtNome.innerHTML = 'Nome Inválido'
-        txtNome.classList = 'alert alert-danger'        
-            
+        txtNome.classList = 'alert alert-danger'
+             
     }else{
         txtNome.innerHTML = 'Nome Válido'
         txtNome.classList = 'alert alert-success'
@@ -22,13 +22,17 @@ function validaEmail() {
     let txtEmail = document.querySelector('#txtEmail')
 
     if (email.value.indexOf('@')== -1 || email.value.indexOf('.') == -1) {
+        
         txtEmail.innerHTML = 'E-mail Inválido'
         txtEmail.classList = 'alert alert-danger'
+        
+        
     }else{
         txtEmail.innerHTML = 'E-mail Válido'
-        txtEmail.classList = 'alert alert-success'
+        txtEmail.classList = 'alert alert-success'       
         emailOk =  true 
     }
+
 }
 
 function validaAssunto() {
@@ -37,6 +41,7 @@ function validaAssunto() {
     if (assunto.value.length > 100) {
         txtAssunto.innerHTML = 'Testo é muito grande, digite no máximo 100 caracteres'
         txtAssunto.classList = 'alert alert-danger'
+        
         txtAssunto.style.display = 'block'
     }else{
         txtAssunto.style.display = 'none'
